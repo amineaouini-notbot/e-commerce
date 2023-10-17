406a9eae412012a7c0c29b5a8f0a7c127c6120f2
amine m. aouini <aminaouini565@gmail.com>
2023-10-18 00:19:07 +0100

    hash pass on signup

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const db = require('./db/db')
 const userRouter = require('./routes/user')
+
+app.use(express.urlencoded({ extended: false}))
 app.set('views', './views')
 app.set('view engine', 'ejs')
-
 app.get('/', (req, res)=>{
     res.render('Home.ejs')
 })
 
 app.use('/user', userRouter)
-const PORT = 3000
+const PORT = 5000
 app.listen(PORT, ()=>{
-    console.log('server works on port 3000!')
+    console.log('server works on port 5000!')
 })
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,24 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
 
 router.get('/signup', (req, res)=>{
-    res.render('signup')
+    res.render('signup', {username: '', emai: '', password: ''})
 })
 
+router.post('/signup', (req, res)=>{
+    console.log(req.body, 'test')
+    bcrypt.hash(req.body.password, 10, (err, hash)=>{
+        if (err) throw err;
+
+        let newClient = {
+            username: req.body.username,
+            emai: req.body.emai,
+            password: hash
+        }
+        console.log(newClient)
+    })
+    
+
+    res.send('signedupdd')
+})
 module.exports = router;
