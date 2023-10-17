f18dc70b9bb63b21bef28ad9ad86ee92dd850d39
amine m. aouini <aminaouini565@gmail.com>
2023-10-17 23:11:11 +0100

    setup user routes with signup front

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require('express')();
 const db = require('./db/db')
-
+const userRouter = require('./routes/user')
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
     res.render('Home.ejs')
 })
+
+app.use('/user', userRouter)
 const PORT = 3000
 app.listen(PORT, ()=>{
     console.log('server works on port 3000!')
