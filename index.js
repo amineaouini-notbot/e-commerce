6707e267c9da2df5bcba49ccd2a089f7810c4447
amine m. aouini <aminaouini565@gmail.com>
2023-10-19 00:06:46 +0100

    setup env

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const db = require('./db/db')
 const userRouter = require('./routes/user')
 
+require('dotenv').config()
+
 app.use(express.urlencoded({ extended: false}))
 app.set('views', './views')
 app.set('view engine', 'ejs')
+
 app.get('/', (req, res)=>{
     res.render('Home.ejs')
 })
