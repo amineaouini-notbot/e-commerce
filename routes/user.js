bb7399250f613659aba3ae9ac4ea425ffcdf90f9
amine m. aouini <aminaouini565@gmail.com>
2023-10-18 23:04:44 +0100

    insert new client into db

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,23 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const db = require('../db/db')
 
 router.get('/signup', (req, res)=>{
     res.render('signup', {username: '', emai: '', password: ''})
 })
 
 router.post('/signup', (req, res)=>{
-    console.log(req.body, 'test')
+    console.log(req.body)
     bcrypt.hash(req.body.password, 10, (err, hash)=>{
         if (err) throw err;
-
-        let newClient = {
-            username: req.body.username,
-            emai: req.body.emai,
-            password: hash
-        }
-        console.log(newClient)
+        
+        const {username, email} = req.body;
+        db.query('INSERT INTO client (username, email, password, created_at) VALUES (?, ?, ?, ?)', 
+            [username, email, hash, new Date()],
+            (err, result)=>{
+                if (err) throw err;
+                console.log(result.insertId)
+            })
     })
     
 
