e36018c693fbd4991d86c66566ee61514bf65992
amine m. aouini <aminaouini565@gmail.com>
2023-10-19 22:56:09 +0100

    verify token with session

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next)=>{
 
-    const token = req.header('auth-token')
+    const {token} = req.session
     if (!token) res.status(401).render('signup', {username: '', emai: '', password: ''})
 
     try {
