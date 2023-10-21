c39c7234ab188a6dc842e77ba5f8a203f05eab48
amine m. aouini <aminaouini565@gmail.com>
2023-10-21 23:25:15 +0100

    setup login as admin back

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const db = require('../db/db')
 
 router.get('/', (req, res)=>{
     if (req.session.adminLogged) {res.redirect('/admin/login')}
@@ -11,4 +12,19 @@ router.get('/login', (req, res)=>{
     else res.render('adminLogin')
 })
 
+router.post('/login', (req, res)=>{
+    let {username, password} = req.body
+    db.query('SELECT id FROM admin WHERE username = (?) AND password = UNHEX(md5(?))',
+        [username, password],
+        (err, result) =>{
+            if (err) { res.send(`You can't login as Admin!`)}
+            else {
+                let privateKey = process.env.TOKEN_PASS;
+                let token = jwt.sign({ _id: result.insertId }, privateKey);
+                req.session.adminLogged = token
+                res.redirect('/admin')
+            }
+        } 
+    )
+})
 module.exports = router
