95b09d07b30227a9f4125bd15f1758610c229b06
amine m. aouini <aminaouini565@gmail.com>
2023-10-21 15:47:29 +0100

    setup router admin when logged

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.get('/', (req, res)=>{
+    if (req.session.adminLogged) {res.redirect('/admin/login')}
+    else res.send('logged as admin')
+})
+
+
 router.get('/login', (req, res)=>{
     if (req.session.adminLogged) {res.redirect('/admin')}
     else res.render('adminLogin')
