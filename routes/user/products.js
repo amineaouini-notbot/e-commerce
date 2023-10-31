79a33b5c652edaa714164fb9e392aa78c4d7edf4
amine m. aouini <aminaouini565@gmail.com>
2023-10-31 21:29:54 +0100

    setup produts by id route for front

--- routes/user/products.js
+++ routes/user/products.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
+const db = require('../../db/db');
 const verifyToken = require('../verifyToken')
+const fs = require('fs')
 
 router.get("/byCateg/:id", verifyToken, (req, res)=>{
     if(!req.session.categories && !req.session.products) res.redirect('/user')
@@ -15,4 +17,22 @@ router.get("/byCateg/:id", verifyToken, (req, res)=>{
     }
 })
 
+router.get('/:id', verifyToken, (req, res)=>{
+    if(!req.session.categories && !req.session.products) res.redirect('/user')
+    else {
+        let {id} = req.params
+        db.query('SELECT * FROM product WHERE id = ?', [id], (err, result)=>{
+            if(err) res.send('problem accured in db')
+            else{
+                if(result[0]){
+                    let product = result[0]
+                    const imagesList = fs.readdirSync(__dirname+'/../../public/upload/' + id)
+                    product.images = imagesList
+                    res.render('product', {product})
+                }
+            }
+        })
+    }
+    
+})
 module.exports = router
