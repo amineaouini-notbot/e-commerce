9ecda53663cc97b7fd3b4a79eef017a01c147587
amine m. aouini <aminaouini565@gmail.com>
2023-11-08 22:58:01 +0100

    setup connection to hosted db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
 let mysql = require('mysql')
+let mysql2 = require('mysql2')
+const {DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME} = process.env
 
-var db = mysql.createConnection({
+var db = DB_HOST ? mysql2.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  port: DB_PORT,  
+}) : mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "123456",
